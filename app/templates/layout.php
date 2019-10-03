9bd28e4f45cfee2f242846b089c93aaf0e1caeee
pierre <pm@enyopharma.com>
2019-10-03 10:50:06 +0200

    fix layout

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -4,10 +4,10 @@
         <title>
             <?= isset($title) ? $this->e($title) : 'Welcome' ?>
         </title>
-        <link rel="stylesheet" href="<?= $this->asset('build/app.css') ?>" />
+        <link rel="stylesheet" href="<?= $this->asset('app.css') ?>" />
         <?= $this->section('styles') ?>
-        <script type="text/javascript" src="<?= $this->asset('build/runtime.js') ?>"></script>
-        <script type="text/javascript" src="<?= $this->asset('build/app.js') ?>"></script>
+        <script type="text/javascript" src="<?= $this->asset('runtime.js') ?>"></script>
+        <script type="text/javascript" src="<?= $this->asset('app.js') ?>"></script>
         <?= $this->section('scripts') ?>
     </head>
     <body>
