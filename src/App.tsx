98e1fb1d03a62739d2d5120981896991e115e5a2
pierre <pm@enyopharma.com>
2019-12-09 17:00:06 +0100

    base router

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import 'bootstrap/js/dist/collapse';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { Navbar } from './components/Navbar'
+import { HomePage } from './components/HomePage'
+import { SearchPage } from './components/SearchPage';
 
 const App: React.FC = () => {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-                <div className="container">
-                    <a className="navbar-brand" href="/">Vinland</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navcontent">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div id="navcontent" className="collapse navbar-collapse">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+        <BrowserRouter>
+            <Navbar />
             <div className="container">
+                <Switch>
+                    <Route exact path="/">
+                        <HomePage />
+                    </Route>
+                    <Route path="/interactions">
+                        <SearchPage />
+                    </Route>
+                </Switch>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/SearchPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SearchPage: React.FC = () => (
+    <h1>Search for interactions</h1>
+)
+
+export { SearchPage }
