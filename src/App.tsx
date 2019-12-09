9d9da9297ad7c401d0c5a701774c09232a830cec
pierre <pm@enyopharma.com>
2019-12-09 16:40:12 +0100

    setup vinland theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/js/dist/collapse';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+                <div className="container">
+                    <a className="navbar-brand" href="/">Vinland</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navcontent">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div id="navcontent" className="collapse navbar-collapse">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div className="container">
+            </div>
+        </div>
+    );
 }
 
 export default App;
