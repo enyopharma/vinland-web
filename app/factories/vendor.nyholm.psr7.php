15420889df0553eaac6e0940c2a3e45080fd7484
pierre <pm@enyopharma.com>
2019-12-18 17:36:47 +0100

    minor change psr17 factories

--- app/factories/vendor.nyholm.psr7.php
+++ app/factories/vendor.nyholm.psr7.php
@@ -8,9 +8,10 @@ use Psr\Http\Message\ResponseFactoryInterface;
 
 use Nyholm\Psr7\Factory\Psr17Factory;
 
+$factory = new Psr17Factory;
+
 return [
-    Psr17Factory::class => fn () => new Psr17Factory,
-    UriFactoryInterface::class => fn ($c) => $c->get(Psr17Factory::class),
-    StreamFactoryInterface::class => fn ($c) => $c->get(Psr17Factory::class),
-    ResponseFactoryInterface::class => fn ($c) => $c->get(Psr17Factory::class),
+    UriFactoryInterface::class => fn () => $factory,
+    StreamFactoryInterface::class => fn () => $factory,
+    ResponseFactoryInterface::class => fn () => $factory,
 ];
