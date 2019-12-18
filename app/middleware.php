8ae58cc6c04814a15a948602551546a71379afe4
pierre <pm@enyopharma.com>
2019-12-18 13:06:43 +0100

    use middlewares/cors

--- app/middleware.php
+++ app/middleware.php
@@ -11,14 +11,22 @@ use Psr\Container\ContainerInterface;
  * @return Psr\Http\Server\MiddlewareInterface[]
  */
 return function (ContainerInterface $container): array {
+    /**
+     * CORS settings.
+     */
+    $settings = (new Neomerx\Cors\Strategies\Settings)
+        ->init('http', $_ENV['APP_HOST'], 80)
+        ->setAllowedOrigins(explode(',', $_ENV['ALLOWED_ORIGINS']))
+        ->setAllowedMethods(['GET', 'POST'])
+        ->setAllowedHeaders(['content-type']);
+
     return [
         /**
          * Cross-origin resource sharing middleware.
          */
-        new App\Http\Middleware\CORSMiddleware(
+        new Middlewares\Cors(
+            Neomerx\Cors\Analyzer::instance($settings),
             $container->get(Psr\Http\Message\ResponseFactoryInterface::class),
-            ['GET', 'POST'],
-            ...explode(',', $_ENV['ALLOWED_DOMAINS'])
         ),
 
         /**
@@ -31,7 +39,7 @@ return function (ContainerInterface $container): array {
          */
         new App\Http\Middleware\FastRouteMiddleware(
             $container->get(Psr\Http\Message\ResponseFactoryInterface::class),
-            $container->get(FastRoute\Dispatcher::class)
+            $container->get(FastRoute\Dispatcher::class),
         ),
     ];
 };
