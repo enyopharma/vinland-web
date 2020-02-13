0d4e4f3f4a481da7c6bb500f9c6fc71bff93cd39
pierre <pm@enyopharma.com>
2020-02-13 12:32:57 +0100

    fix bug with viral name (state2query Array.sort() was mutating the state)

--- src/app/search/utils.ts
+++ src/app/search/utils.ts
@@ -3,8 +3,13 @@ import { AppSelector } from 'app'
 import { Query } from 'features/query'
 import { parse as parseIdentifiers } from 'features/identifiers'
 
+/**
+ * Caution: Array.sort() mutates the array which is not allowed.
+ * Need to clone identifiers and names (parseIdentifiers() or Array.slice()) before sorting.
+ */
 export const state2query: AppSelector<Query> = state => {
     const identifiers = parseIdentifiers(state.search.identifiers)
+    const names = state.search.taxonomy.names.slice()
     const ncbi_taxon_id = state.search.taxonomy.taxon === null ? 0 : state.search.taxonomy.taxon.ncbi_taxon_id
 
     const parts: string[] = []
@@ -14,14 +19,14 @@ export const state2query: AppSelector<Query> = state => {
     parts.push(state.search.options.publications.toString())
     parts.push(state.search.options.methods.toString())
     parts.push(ncbi_taxon_id.toString())
-    parts.push(...state.search.taxonomy.names.sort((a, b) => a.localeCompare(b)))
+    parts.push(...names.sort((a: string, b: string) => a.localeCompare(b)))
     parts.push(...identifiers.sort((a: string, b: string) => a.localeCompare(b)))
 
     return {
         key: md5(parts.join(':')),
         identifiers: identifiers,
         ncbi_taxon_id: ncbi_taxon_id,
-        names: state.search.taxonomy.names,
+        names: names,
         hh: state.search.options.hh,
         vh: state.search.options.vh,
         neighbors: state.search.options.neighbors,
