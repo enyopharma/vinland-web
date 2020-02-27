354709ffa91ff3ab28ac9c6928aa1554539ad3f1
pierre <pm@enyopharma.com>
2020-02-27 10:13:58 +0100

    add human and viral protein tabs

--- src/features/query/components/QueryResultCard.tsx
+++ src/features/query/components/QueryResultCard.tsx
@@ -3,11 +3,12 @@ import React, { Suspense, useState, useEffect } from 'react'
 import { ResultWrapper } from 'features/query'
 
 const Pagination = React.lazy(() => import('./Pagination').then(module => ({ default: module.Pagination })))
+const ProteinCardTable = React.lazy(() => import('./ProteinCardTable').then(module => ({ default: module.ProteinCardTable })))
 const InteractionCardTable = React.lazy(() => import('./InteractionCardTable').then(module => ({ default: module.InteractionCardTable })))
 const NetworkControlCardBody = React.lazy(() => import('./NetworkControlCardBody').then(module => ({ default: module.NetworkControlCardBody })))
 const NetworkStageCardBody = React.lazy(() => import('./NetworkStageCardBody').then(module => ({ default: module.NetworkStageCardBody })))
 
-type Tab = 'list' | 'network'
+type Tab = 'list' | 'human' | 'virus' | 'network'
 
 type Props = {
     result: ResultWrapper
@@ -32,6 +33,24 @@ export const QueryResultCard: React.FC<Props> = ({ result }) => {
                             onClick={getHandleClick('list')}
                         >
                             List view
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            href="/"
+                            className={tab === 'human' ? 'nav-link active' : 'nav-link'}
+                            onClick={getHandleClick('human')}
+                        >
+                            Human proteins
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            href="/"
+                            className={tab === 'virus' ? 'nav-link active' : 'nav-link'}
+                            onClick={getHandleClick('virus')}
+                        >
+                            Viral proteins
                         </a>
                     </li>
                     <li className="nav-item">
@@ -64,31 +83,71 @@ const ProgressBarCardBody = () => (
 )
 
 const CardBody: React.FC<{ tab: Tab } & Props> = ({ tab, result }) => {
-    const [offset, setOffset] = useState(0)
+    const limit = 20
+
+    const [offseti, setOffseti] = useState(0)
+    const [offseth, setOffseth] = useState(0)
+    const [offsetv, setOffsetv] = useState(0)
     const [ratio, setRatio] = useState(100)
     const [labels, setLabels] = useState(false)
 
-    useEffect(() => { setOffset(0) }, [result])
+    useEffect(() => { setOffseti(0) }, [result])
+    useEffect(() => { setOffseth(0) }, [result])
+    useEffect(() => { setOffsetv(0) }, [result])
     useEffect(() => { setRatio(100) }, [result])
     useEffect(() => { setLabels(false) }, [result])
 
     switch (tab) {
-        case 'list':
-            const limit = 20
-            const interactions = result.interactions.slice(offset, offset + limit);
+        case 'list': {
+            const interactions = result.interactions.slice(offseti, offseti + limit);
 
             return (
                 <React.Fragment>
                     <div className="card-body">
-                        <Pagination offset={offset} limit={limit} total={result.interactions.length} update={setOffset} />
+                        <Pagination offset={offseti} limit={limit} total={result.interactions.length} update={setOffseti} />
                     </div>
                     <InteractionCardTable interactions={interactions} limit={limit} />
                     <div className="card-body">
-                        <Pagination offset={offset} limit={limit} total={result.interactions.length} update={setOffset} />
+                        <Pagination offset={offseti} limit={limit} total={result.interactions.length} update={setOffseti} />
                     </div>
                 </React.Fragment>
             )
-        case 'network':
+        }
+        case 'human': {
+            const proteins = result.proteins.human()
+
+            const slice = proteins.slice(offseth, offseth + limit)
+
+            return (
+                <React.Fragment>
+                    <div className="card-body">
+                        <Pagination offset={offseth} limit={limit} total={proteins.length} update={setOffseth} />
+                    </div>
+                    <ProteinCardTable proteins={slice} limit={limit} />
+                    <div className="card-body">
+                        <Pagination offset={offseth} limit={limit} total={proteins.length} update={setOffseth} />
+                    </div>
+                </React.Fragment>
+            )
+        }
+        case 'virus': {
+            const proteins = result.proteins.virus()
+
+            const slice = proteins.slice(offsetv, offsetv + limit)
+
+            return (
+                <React.Fragment>
+                    <div className="card-body">
+                        <Pagination offset={offsetv} limit={limit} total={proteins.length} update={setOffsetv} />
+                    </div>
+                    <ProteinCardTable proteins={slice} limit={limit} />
+                    <div className="card-body">
+                        <Pagination offset={offsetv} limit={limit} total={proteins.length} update={setOffsetv} />
+                    </div>
+                </React.Fragment>
+            )
+        }
+        case 'network': {
             const network = result.network()
 
             return (
@@ -103,6 +162,7 @@ const CardBody: React.FC<{ tab: Tab } & Props> = ({ tab, result }) => {
                     <NetworkStageCardBody network={network} />
                 </React.Fragment>
             )
+        }
         default:
             throw new Error()
     }
--- src/features/query/wrapper.ts
+++ src/features/query/wrapper.ts
@@ -1,10 +1,36 @@
-import { SuccessfulQueryResult, Network } from './types'
+import { SuccessfulQueryResult, Protein, Interaction, Network } from './types'
 import { network } from './network'
 
 export const wrapper = (result: SuccessfulQueryResult) => {
     return {
         interactions: result.interactions,
+        proteins: {
+            human: getProteinCache('h', result),
+            virus: getProteinCache('v', result),
+        },
         network: getNetworkCache(result),
+    }
+}
+
+const filter = (type: Protein['type'], interactions: Interaction[]) => {
+    const map: Record<string, Protein> = {}
+    interactions.forEach(i => {
+        if (i.protein1.type === type) map[`${i.protein1.accession}:${i.protein1.name}`] = i.protein1
+        if (i.protein2.type === type) map[`${i.protein2.accession}:${i.protein2.name}`] = i.protein2
+    })
+    return Object.values(map)
+}
+
+const getProteinCache = (type: Protein['type'], result: SuccessfulQueryResult) => {
+    let cache: Protein[] | null = null
+
+    return () => {
+        if (cache === null) throw new Promise((resolve => setTimeout(() => {
+            cache = filter(type, result.interactions)
+            resolve()
+        }, 0)))
+
+        return cache
     }
 }
 
@@ -12,12 +38,10 @@ const getNetworkCache = (result: SuccessfulQueryResult) => {
     let cache: Network | null = null
 
     return () => {
-        if (cache === null) {
-            throw new Promise(resolve => setTimeout(() => {
-                cache = network(result.interactions)
-                resolve()
-            }, 0))
-        }
+        if (cache === null) throw new Promise(resolve => setTimeout(() => {
+            cache = network(result.interactions)
+            resolve()
+        }, 0))
 
         return cache
     }
