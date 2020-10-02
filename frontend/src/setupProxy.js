971ba6413afb3f7d0c3a38c23d52360af057ae8a
pierre <pm@enyopharma.com>
2020-10-02 16:45:54 +0200

    remove useless env example from frontend

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use('/api', proxy({
-        target: process.env.REACT_APP_API_HOST,
+        target: 'http://localhost',
         changeOrigin: true,
         pathRewrite: { '^/api': '' },
     }));
