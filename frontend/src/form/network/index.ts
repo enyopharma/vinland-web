a212a0ebb350fdce3d6c02da1def4b76ce3ed284
pierre <pm@enyopharma.com>
2020-11-23 14:52:45 +0100

    put function creating network from interactions in factory file

--- frontend/src/form/network/index.ts
+++ frontend/src/form/network/index.ts
@@ -1,11 +1,11 @@
 import Konva from 'konva'
 import { config } from '../config'
-import { Interaction, Protein } from '../types'
+import { Interaction } from '../types'
 import { Node, Link } from './types'
 import { getLabels } from './labels'
+import { getNetwork } from './factory'
 import { getSelection } from './selection'
 import { getSimulation } from './simulation'
-import { getColorPicker } from './color'
 
 const style = {
     nodes: {
@@ -38,59 +38,9 @@ const style = {
 }
 
 export const network = (interactions: Interaction[]) => {
-    return setUpStage(getNodesAndLinks(interactions))
-}
-
-const getNodesAndLinks = (interactions: Interaction[]) => {
-    const nodes: Node[] = []
-    const links: Link[] = []
-    const map: Record<string, Node> = {}
-    const associations: string[] = []
-    const color = getColorPicker()
-
-    const registerNode = (protein: Protein) => {
-        const nodeid = `${protein.species.ncbi_taxon_id}:${protein.name}`
-        const proteinid = `${protein.accession}:${protein.name}`
-
-        let node = map[nodeid] ?? null
-
-        if (node === null) nodes.push(node = map[nodeid] = {
-            id: nodeid,
-            data: {
-                type: protein.type,
-                name: protein.name,
-                color: color(protein),
-                species: protein.species.name,
-                proteins: {},
-            },
-            selection: {},
-        })
-
-        if (!node.data.proteins[proteinid]) {
-            node.data.proteins[proteinid] = protein
-        }
-
-        return node
-    }
-
-    interactions.forEach(interaction => {
-        const node1 = registerNode(interaction.protein1)
-        const node2 = registerNode(interaction.protein2)
-
-        if (!associations.includes(`${node1.id}:${node2.id}`)) {
-            links.push({ source: node1, target: node2, selection: {} })
-
-            associations.push(`${node1.id}:${node2.id}`)
-            associations.push(`${node2.id}:${node1.id}`)
-        }
-    })
-
-    return { nodes, links }
-}
-
-const setUpStage = (network: { nodes: Node[], links: Link[] }) => {
-    const ui = { labels: getLabels(), selection: getSelection(network) }
+    const network = getNetwork(interactions)
     const simulation = getSimulation(network)
+    const ui = { labels: getLabels(), selection: getSelection(network) }
     const listeners: Array<() => void> = []
 
     /**
