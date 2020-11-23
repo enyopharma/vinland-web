025fb5b489d8fd78cc037157789406925564e0be
pierre <pm@enyopharma.com>
2020-11-23 11:43:41 +0100

    add mapping modal

--- frontend/src/pages/components/MappingImg.tsx
+++ frontend/src/pages/components/MappingImg.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from 'react-bootstrap4-modal';
 
 import { Mapping } from '../types'
+
+const colors = {
+    h: '#6CC3D5',
+    v: '#FF7851',
+}
+
+const btnclasses = {
+    h: 'btn btn-block btn-info',
+    v: 'btn btn-block btn-danger',
+}
 
 type MappingImgProps = {
     type: 'h' | 'v'
@@ -8,12 +19,19 @@ type MappingImgProps = {
     mappings: Mapping[]
 }
 
-export const MappingImg: React.FC<MappingImgProps> = ({ type, width, mappings }) => (
-    <svg width="100%" height="30">
-        <rect width="100%" y="14" height="2" style={{ fill: '#eee', strokeWidth: 0 }} />
-        {mappings.map((mapping, i) => <MappingRect key={i} type={type} width={width} mapping={mapping} />)}
-    </svg>
-)
+export const MappingImg: React.FC<MappingImgProps> = ({ type, width, mappings }) => {
+    const [visible, setVisible] = useState<boolean>(false)
+
+    return (
+        <React.Fragment>
+            <svg width="100%" height="30" onClick={() => setVisible(true)}>
+                <rect width="100%" y="14" height="2" style={{ fill: '#eee', strokeWidth: 0 }} />
+                {mappings.map((mapping, i) => <MappingRect key={i} type={type} width={width} mapping={mapping} />)}
+            </svg>
+            <MappingModal type={type} visible={visible} mappings={mappings} close={() => setVisible(false)} />
+        </React.Fragment>
+    )
+}
 
 type MappingRectProps = {
     type: 'h' | 'v'
@@ -22,7 +40,6 @@ type MappingRectProps = {
 }
 
 const MappingRect: React.FC<MappingRectProps> = ({ type, width, mapping }) => {
-    const color = type === 'h' ? '#6CC3D5' : '#FF7851'
     const startp = ((mapping.start / width) * 100) + '%'
     const stopp = ((mapping.stop / width) * 100) + '%'
     const widthp = (((mapping.stop - mapping.start + 1) / width) * 100) + '%'
@@ -35,7 +52,35 @@ const MappingRect: React.FC<MappingRectProps> = ({ type, width, mapping }) => {
             <text x={stopp} y="30" textAnchor="end" style={{ fontSize: '10px' }}>
                 {mapping.stop}
             </text>
-            <rect width={widthp} x={startp} y="13" height="4" style={{ fill: color, strokeWidth: 0 }} />
+            <rect width={widthp} x={startp} y="13" height="4" style={{ fill: colors[type], strokeWidth: 0 }} />
         </React.Fragment>
     )
 }
+
+type MappingModalProps = {
+    type: 'h' | 'v'
+    visible: boolean
+    mappings: Mapping[]
+    close: () => void
+}
+
+const MappingModal: React.FC<MappingModalProps> = ({ type, visible, mappings, close }) => (
+    <Modal visible={visible} dialogClassName="modal-lg modal-dialog-centered" onClickBackdrop={close}>
+        <div className="modal-header">
+            <h5 className="modal-title">Mappings</h5>
+        </div>
+        {mappings.map((mapping, i) => (
+            <div key={i} className="modal-body text-left">
+                <h6>Sequence [{mapping.start} - {mapping.stop}]</h6>
+                <div className="form-row">
+                    <textarea value={mapping.sequence} className="form-control" readOnly={true} />
+                </div>
+            </div>
+        ))}
+        <div className="modal-footer">
+            <button type="button" className={btnclasses[type]} onClick={() => close()}>
+                close
+        </button>
+        </div>
+    </Modal>
+)
