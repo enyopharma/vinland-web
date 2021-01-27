34f76bac0320dbeafeb6ff295a3c3c84692fdc88
pierre <pm@enyopharma.com>
2021-01-27 15:30:45 +0100

    remove set timeout

--- frontend/src/form/cache.ts
+++ frontend/src/form/cache.ts
@@ -9,26 +9,23 @@ export const cache = (result: SuccessfulQueryResult) => {
     }
 }
 
+const proteins = (interactions: Interaction[]) => {
+    const proteins: Record<string, Protein> = {}
+
+    interactions.forEach(i => {
+        proteins[`${i.protein1.accession}:${i.protein1.name}`] = i.protein1
+        proteins[`${i.protein2.accession}:${i.protein2.name}`] = i.protein2
+    })
+
+    return Object.values(proteins)
+}
+
 const getProteinCache = (interactions: Interaction[]) => {
     let cache: Protein[] | null = null
 
-    const filter = (interactions: Interaction[]) => {
-        const proteins: Record<string, Protein> = {}
-
-        interactions.forEach(i => {
-            proteins[`${i.protein1.accession}:${i.protein1.name}`] = i.protein1
-            proteins[`${i.protein2.accession}:${i.protein2.name}`] = i.protein2
-        })
-
-        return Object.values(proteins)
-    }
-
     return () => {
         if (cache === null) {
-            throw new Promise(resolve => setTimeout(() => {
-                cache = filter(interactions)
-                resolve()
-            }, 0))
+            throw new Promise<Protein[]>(resolve => resolve(proteins(interactions))).then(proteins => cache = proteins)
         }
 
         return cache
@@ -40,10 +37,7 @@ const getNetworkCache = (interactions: Interaction[]) => {
 
     return () => {
         if (cache === null) {
-            throw new Promise(resolve => setTimeout(() => {
-                cache = network(interactions)
-                resolve()
-            }, 0))
+            throw new Promise<Network>(resolve => resolve(network(interactions))).then(network => cache = network)
         }
 
         return cache
