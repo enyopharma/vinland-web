832b7a1f9da60f70d001d0c21b34b720c431868d
pmall <pierre.mallinjoud@gmail.com>
2023-10-12 10:23:23 +0200

    fix bug php depreciation

--- app/src/ReadModel/Statement.php
+++ app/src/ReadModel/Statement.php
@@ -59,7 +59,7 @@ final class Statement implements \IteratorAggregate
         return iterator_to_array($this->iterator);
     }
 
-    public function getIterator(): \Iterator
+    public function getIterator(): \Traversable
     {
         return $this->iterator;
     }
